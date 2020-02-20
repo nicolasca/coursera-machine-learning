8529d61866fd12870c59420cac38d109fd24f6ed
ncastejon <castejon.nicolas@gmail.com>
2020-02-20 14:13:58 +0100

    Finish week 2

--- week2/machine-learning-ex1/ex1/ex1_multi.m
+++ week2/machine-learning-ex1/ex1/ex1_multi.m
@@ -82,16 +82,35 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.05;
+alpha = 0.01;
 num_iters = 400;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
-[theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
+[theta, J_history1] = gradientDescentMulti(X, y, theta, alpha, num_iters);
+
+alpha = 0.03;
+theta = zeros(3, 1);
+[theta, J_history2] = gradientDescentMulti(X, y, theta, alpha, num_iters);
+
+alpha = 0.1;
+theta = zeros(3, 1);
+[theta, J_history3] = gradientDescentMulti(X, y, theta, alpha, num_iters);
+
+alpha = 0.3;
+theta = zeros(3, 1);
+[theta, J_history4] = gradientDescentMulti(X, y, theta, alpha, num_iters);
 
 % Plot the convergence graph
 figure;
-plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
+plot(1:numel(J_history1), J_history1, '-b', 'LineWidth', 2);
+hold on
+
+plot(1:numel(J_history2), J_history2, '-r', 'LineWidth', 2);
+plot(1:numel(J_history3), J_history3, '-y', 'LineWidth', 2);
+plot(1:numel(J_history4), J_history4, '-k', 'LineWidth', 2);
+
+
 xlabel('Number of iterations');
 ylabel('Cost J');
 
@@ -104,8 +123,8 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-norm = ([1650,3] - mu) / sigma;
-price = [1, norm(1), norm(2)] * theta; % You should change this
+norm = ([1650,3] - mu) ./ sigma;
+price = [1 norm] * theta; % You should change this
 
 
 % ============================================================
@@ -150,7 +169,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = [1, 1650, 3] * theta; % You should change this
 
 
 % ============================================================
